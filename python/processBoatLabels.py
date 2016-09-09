2c242293745bbf727e94876cf139ea43e481303d
dlindenbaum <dlindenbaum@iqt.org>
2016-09-09 18:41:19 -0400

    processBoatLabels.py made for spawar

--- python/processBoatLabels.py
+++ python/processBoatLabels.py
@@ -1,6 +1,7 @@
-import evalTools as eT
-import geoTools as gT
-import labelTools as lT
+from spaceNet import evalTools as eT
+from spaceNet import geoTools as gT
+from spaceNet import labelTools as lT
+from spaceNet import dataTools as dT
 import numpy as np
 import sys
 import multiprocessing
@@ -16,4 +17,17 @@ import math
 
 
 if __name__ == "__main__":
-    
+
+    rasterFileName = '/Users/dlindenbaum/dataStorage/dgData/panamaCanal/Validation/WV02_10032015/wv02_10032015_R2C2_mask.tif'
+    shapeFile = '/Users/dlindenbaum/dataStorage/dgData/panamaCanal/Validation/combinedTruthLine/TruthLine_BoatOnly_PanamaCanalValidate.shp'
+    outputDirectory = '/Users/dlindenbaum/dataStorage/dgData/test'
+
+    newGeoJson = shapeFile.replace('.shp', "full.geojson")
+    lT.convertLabelStringToPoly(shapeFileSrc=shapeFile,outGeoJSon=shapeFile.replace('.shp', "full.geojson"), labelType='Boat')
+    dT.chipImage(rasterFileName,
+                 newGeoJson,
+                 outputDirectory,
+                 finalImageSize=256,
+                 rotationList=np.arange(0,360,5),
+                 rotateNorth=True,
+                 windowSize='adjust')
